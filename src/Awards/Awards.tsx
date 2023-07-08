324a5df157e96a9e7f9ba4108c9c0056a1a9bddc
Michael Lee <mlee@michaels-mbp.mynetworksettings.com>
2023-07-08 11:12:08 -0400

    finished awards page

--- src/Awards/Awards.tsx
+++ src/Awards/Awards.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import AwardsDesc from "./AwardsDesc";
 import TiersAwards from "./TiersAwards";
-
+import AwardsDescPlatinum from "./AwardsDescPlatinum";
 const Awards = () => {
     return (
         <div className="awards-container">
             <AwardsDesc/>
             <TiersAwards/>
+            <AwardsDescPlatinum/>
         </div>
     );
 }
--- src/Awards/TiersAwards.tsx
+++ src/Awards/TiersAwards.tsx
@@ -10,11 +10,11 @@ const TiersAwards = () => {
                     Gold, Silver, and Bronze Braces
                 </header>
                 <p>
-                These awards are awarded to individuals in each category.
-                Gold winners from each category will proceed to the final
-                round and have an opportunity to win the Platinum award.
-                All winners receive a cash
-                prize and will have their designs included in the upcoming Friddles Back Brace Line.
+                    These awards are awarded to individuals in each category.
+                    Gold winners from each category will proceed to the final
+                    round and have an opportunity to win the Platinum award.
+                    All winners receive a cash
+                    prize and will have their designs included in the upcoming Friddles Back Brace Line.
                 </p>
                 <br/>
                 <Row className = "tiers-awards-description">
@@ -22,11 +22,10 @@ const TiersAwards = () => {
                         Golden Brace
                     </Col>
                     <Col className = "tiers-awards-description-desc">
-                    Winners who receive a Golden Brace award will receive a
-                    $500 prize and have their
-                    <br/>
-                    work entered into the final round across all categories. Their design will also be included in the upcoming Friddles Back Brace Line.
-                    There are two Golden Brace awards that will be given out.
+                        Winners who receive a Golden Brace award will receive a
+                        $500 prize and have their
+                        work entered into the final round across all categories. Their design will also be included in the upcoming Friddles Back Brace Line.
+                        There are two Golden Brace awards that will be given out.
                     </Col>
                     <br/>
                 </Row>
@@ -36,9 +35,9 @@ const TiersAwards = () => {
                         Silver Brace
                     </Col>
                     <Col className = "tiers-awards-description-desc">
-                    Winners who receive a Silver Brace Award will receive a $250 prize.
-                    Their design will also be included in the upcoming Friddles Back Brace Line.
-                    There are four Silver Brace awards that will be given out.
+                        Winners who receive a Silver Brace Award will receive a $250 prize.
+                        Their design will also be included in the upcoming Friddles Back Brace Line.
+                        There are four Silver Brace awards that will be given out.
                     </Col>
                 </Row>
                 <br/>
@@ -47,10 +46,10 @@ const TiersAwards = () => {
                         Bronze Brace
                     </Col>
                     <Col className = "tiers-awards-description-desc">
-                    Winners who receive a Bronze Brace Award will receive a $250 prize.
-                    Their design will also be included in
-                    the upcoming Friddles Back Brace Line.
-                    There are four Silver Brace awards that will be given out.
+                        Winners who receive a Bronze Brace Award will receive a $250 prize.
+                        Their design will also be included in
+                        the upcoming Friddles Back Brace Line.
+                        There are four Silver Brace awards that will be given out.
                     </Col>
                 </Row>
             </div>
